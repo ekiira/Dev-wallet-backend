a760ee9ed72abce143cb85e452e6d0ecd8594a95
Tosin Jimoh <jayoluwatosin@gmail.com>
2020-06-18 23:45:28 +0100

    Updates

--- models/intern.js
+++ models/intern.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 
 const internSchema = new mongoose.Schema({
-  name: `String`,
-  number: 'String',
-  provider: 'String',
-  amount: 'String'
+  name: { type: String, require: true},
+  number: { type: String, require: true, unique: true },
+  provider: { type: String, require: true},
+  amount: { type: String, require: true}
 })
+ 
 
 module.exports = mongoose.model('Intern', internSchema);
